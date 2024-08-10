96e602a7e920d588aab882ab27a5129257fe1fdb
Gregory <gregorymartinsfagundes@hotmail.com>
2024-08-10 20:54:58 -0300

    feat: Realizando tarefas solicitadas no TODO

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,33 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private readonly string _modelo;
+        private readonly string _imei;
+        private readonly int _memoria;
 
-        public Smartphone(string numero)
+        public string Numero { get; set; }
+
+        private string Modelo
+        {
+            get { return _modelo; }
+        }
+
+        private string Imei
+        {
+            get { return _imei; }
+        }
+
+        private int Memoria
+        {
+            get { return _memoria; }
+        }
+
+        protected Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            _modelo = modelo;
+            _imei = imei;
+            _memoria = memoria;
         }
 
         public void Ligar()
@@ -22,5 +42,14 @@ namespace DesafioPOO.Models
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine("# Informações sobre o Smartphone:");
+            Console.WriteLine($"# Número: {Numero}");
+            Console.WriteLine($"# Modelo: {Modelo}");
+            Console.WriteLine($"# IMEI: {Imei}");
+            Console.WriteLine($"# Memória: {Memoria}GB");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,47 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+Console.WriteLine("# Smartphone Iphone:");
+Iphone iphone = new Iphone("123456789", "iPhone 1", "1111111111", 128);
+iphone.ExibirInformacoes();
+
+Console.WriteLine();
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine();
+
+// A única propriedade possível de ser alterada fora do construtor é o Número.
+iphone.Numero = "123-456-789";
+// iphone.Modelo = "iPhone 2";
+// iphone.Imei = "3333333333";
+// iphone.Memoria = 256;
+iphone.ExibirInformacoes();
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("987654321", "Nokia 1", "222222222222", 64);
+nokia.ExibirInformacoes();
+
+Console.WriteLine();
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine();
+
+nokia.Numero = "987-654-321";
+nokia.ExibirInformacoes();
+Console.ResetColor();
